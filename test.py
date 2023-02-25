0ea83b159a05a7acd7b8fd9e9d3daf64d228d5e3
Marco Manduca <marco.manduca95@gmail.com>
2023-02-25 15:20:01 +0100

    update versioning instructions

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 #os.system("mkdir .ssh") #esegue il comando indicato sul terminale
 
+### how to set up github repository for versioning
 """ 
 SSH: sorta di chiave per connettere il terminale in modo sicuro a git
 creare cartella .ssh
@@ -13,7 +14,21 @@ id_rsa e id_rsa.pub
 lanciare il seguente comando per copiare la password da inserire sul sistema di versioning
  pbcopy < ~/.ssh/id_rsa.pub
 
+"""
 
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCo7L0yntTKZIu8bPBwHle4ARvdciLW3gTGLVklkO0xm5EThLlue7sptW0WPuewIegmcjPxFgsulAsVDqCER9BV18iRmLIhZzcd2lSBv26p/3YCEHXMyFuf6R093GETw9K854nXX6ZJmjETyDlbl9448291xFh0dzD45ZChPLtgCuiiVG05z1qIg2oQ+pFzTD7Xk5Yfzuir9i20bl0hcWVD0HKKoiq7Vx65dkNRuxVP3DhqVVLvf6Wd1NdH5jcWxEl6LW2SI9BKYzGMaPxXbPStNAhcFreN/kBx+Mu7FihS4yUJqRTTMAJOU0XLlhU0DpcQuQ9PJeo6l2x/6H6hIrsuy36LLIs3UrvuPTKE9/DwT8hZys9YolkFPSV+/HzvSLCr3VMALezg73Z9oflAB8Qq0rVX6fntY74xJQ8e/6Hsvz5q6aB7DKkybZL706lZRLsv9SXcMtwRINREiAtRfTQI8fT1SYZXyNAMrhUeK9JrYjKK3BFlM/6Utj+BqF4jwoE= marcomanduca@MacBookPro
 
+### git commands
+"""
+
+#git config
+git config --global user.name "Marco Manduca"
+$ git config --global user.email "marco.manduca95@gmail.com"
+$ git config --global push.default matching
+$ git config --global alias.co checkout
+$ git init
+
+#git versioning
+git add .
+git commit -am "commento che descrive le modifiche apportate"
+git push
 """
